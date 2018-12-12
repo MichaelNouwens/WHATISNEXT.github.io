98fdf028787015dac3235784a79aa09b1081dbfa
MichaelNouwens <nouwensmichael0@gmail.com>
2018-12-12 14:14:56 +0200

    Detail Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
-
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using WHATISNEXT.Models;
@@ -26,66 +25,52 @@ namespace WHATISNEXT.Controllers
         public string responseData = null;
         public string prefix = null;
 
-        //    private List<Task<MainPageViewModel>> taskList = new List<Task<MainPageViewModel>>();
-
+      
         // GET: /Home/
         [HttpGet]
         public async Task<ActionResult> Index(string url)
         {
-
-
-
             var prefix = "movie/upcoming";
             var prefix2 = "movie/popular";
 
             using (var httpClient = new HttpClient { BaseAddress = baseAddress })
             {
-
-
                 httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
                 var uri = prefix + "?api_key=" + apiKey + "&language=en-US";
                 var uri2 = prefix2 + "?api_key=" + apiKey + "&language=en-US";
-
-
-
-                //using (var response = await httpClient.GetAsync(uri))
-
                 HttpResponseMessage response = await httpClient.GetAsync(uri);
                 HttpResponseMessage response2 = await httpClient.GetAsync(uri2);
-
                 string responseData = await response.Content.ReadAsStringAsync();
                 string responseData2 = await response2.Content.ReadAsStringAsync();
                 var objResponse1 = JsonConvert.DeserializeObject<upcomingMovies.RootObject>(responseData);
-                var objResponse2 = JsonConvert.DeserializeObject<popularMovies.RootObject>(responseData2);
-                // var objResponse1 = JsonConvert.DeserializeObject<List<TheTMDB.RootObject>>(responseData);
-
-
+                var objResponse2 = JsonConvert.DeserializeObject<popularMovies.RootObject>(responseData2);        
                 var getTotalPages_upcomingmovies = objResponse1.total_pages;
                 var getTotalPages_popularmovies = objResponse2.total_pages;
                 IList<upcomingMovies.Result> lst = objResponse1.results.OfType<upcomingMovies.Result>().ToList();
                 IList<popularMovies.Result> lst2 = objResponse2.results.OfType<popularMovies.Result>().ToList();
-             
 
                 var vm = new MainPageViewModel();
                 vm.UpComingMoviesViewModel = lst;
                 vm.PopularMoviesViewModel = lst2;
                 vm.TotalPagesPopularmovies = getTotalPages_popularmovies;
                 vm.TotalPagesUpcomingmovies = getTotalPages_upcomingmovies;
-
                 return View(vm);
-
-
-                //var CountAllTheUpcomignMovies = objResponse1.total_results;
-                //ViewBag.Count = CountAllTheUpcomignMovies;
-
-
-
-
-
             }
         }
 
-
+        //movie/{movie_id}
+        public async Task<ActionResult> MoreInfo(int id)
+        {         
+            var prefix3 = "movie/" + id.ToString();
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+            {
+                var uri3 = prefix3 + "?api_key=" + apiKey + "&language=en-US";
+                HttpResponseMessage response3 = await httpClient.GetAsync(uri3);
+                string responseData3 = await response3.Content.ReadAsStringAsync();
+                var objResponse3 = JsonConvert.DeserializeObject<detailMovies.RootObject>(responseData3);
+                return View(objResponse3);
+            }     
+        }
 
 
         public ActionResult About()
